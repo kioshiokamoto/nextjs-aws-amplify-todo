7a4749f284830986690d9ed31ca9f22b853b0d0b
Kioshi Okamoto <kjor29@gmail.com>
2021-05-07 15:14:53 -0500

    :dart: IMPROVE: Agregar TODO y autenticacion

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8632413d3685aa3179c3"; }
+/******/ 		__webpack_require__.h = function() { return "05969789312d59c51e85"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
